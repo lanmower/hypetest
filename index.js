82cf6f929e54fd19187d7410723d469d9fd5daa1
lanmower <almagestfraternite@gmail.com>
2023-09-27 01:31:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,40 @@
 const ipc = require('hyper-ipc-secure');
 const goodbye = require('graceful-goodbye');
 require('dotenv').config();
-const webhook = async (inp) => {
-    const outp = { ...inp }
-    const {url, body} = inp;
-    console.log({inp, url, body})
-    await fetch(url,{
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-        body: JSON.stringify(body)
-    })
-    console.log("WEBHOOK", outp)
-    return outp;
-}
+
+import ev from './eval.js'
+const runCall = async (index, calls, input = {}, pk, ipcCall, refresh) => {
+  console.log('run call')
+    const call = calls[index];
+    call.stdout = '';
+    call.stderr = '';
+    console.log('calling before')
+    const before = (await ev(`const console = {log:(...params)=>{api('log', JSON.stringify(params))}}; let params = ${JSON.stringify(paramsWithInput)};${call.before};params`));
+    console.log(before);
+    call.stdout += before.stdout||'';
+    const out = await ipcCall(pk, call.name, before.value);
+    call.stdout += out.stdout||'';
+    call.stderr += out.stderr||'';
+    delete out.stdout;
+    delete out.stderr;
+    const after = (await ev(`const console = {log:(...params)=>{api('log', JSON.stringify(params))}}; let out = ${JSON.stringify(out)};${call.after};out`));
+    const afterOut = after.value;
+    call.stdout += after.stdout||'';
+    call.result = JSON.stringify(afterOut, null, 2)
+    refresh(calls);
+    if (call.output.length) {
+      for (let output of call.output) {
+        await runCall(output.split("-")[1], calls, out, pk, ipcCall, refresh);
+      }
+    }
+    return calls
+  };
 const init = (kp)=>{
     const node = ipc();
     const serverKey = node.getSub(kp, process.env.SERVERNAME);
-    const webhookKey = node.getSub(kp, process.env.IPCNAME);
+    const callKey = node.getSub(kp, process.env.IPCNAME);
   
-    node.lbserve(webhookKey, serverKey,process.env.IPCNAME, webhook);
+    node.lbserve(runCall, serverKey,process.env.IPCNAME, webhook);
     goodbye(node.destroy)
     return node;
 }
