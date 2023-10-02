86a9c29125f1624805b0ade49a1ec38d7d96f6f6
lanmower <almagestfraternite@gmail.com>
2023-10-02 12:27:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,14 @@ const ipc = require('hyper-ipc-secure');
 require('dotenv').config();
 
 import ev from './eval.js'
-const runCall = async (index, calls, input = {}, pk, ipcCall, refresh) => {
-  console.log('run call')
-  const call = calls[index];
-  call.stdout = '';
-  call.stderr = '';
-  console.log('calling before')
-  const before = (await ev(`const console = {log:(...params)=>{api('log', JSON.stringify(params))}}; let params = ${JSON.stringify(input)};${call.before};params`));
-  console.log({ before });
-  call.stdout += before.stdout || '';
-  const out = await ipcCall(pk, call.name, before.value);
-  call.stdout += out.stdout || '';
-  call.stderr += out.stderr || '';
-  delete out.stdout;
-  delete out.stderr;
-  const after = (await ev(`const console = {log:(...params)=>{api('log', JSON.stringify(params))}}; let out = ${JSON.stringify(out)};${call.after};out`));
-  const afterOut = after.value;
-  call.stdout += after.stdout || '';
-  call.result = JSON.stringify(afterOut, null, 2)
-  refresh(calls);
-  if (call.output.length) {
-    for (let output of call.output) {
-      await runCall(output, calls, out, pk, ipcCall, refresh);
-    }
-  }
-  return calls
-};
+const testy = inp => {
+    console.log({ inp });
+    const outp = { ...inp, hello: 'world' };
+    console.log("OUTPUT", outp);
+    return outp;
+}
 const init = (kp, node = ipc(), serverKey = node.getSub(kp, process.env.SERVERNAME), callKey = node.getSub(kp, process.env.IPCNAME)) => {
-  node.lbserve(callKey, serverKey, process.env.IPCNAME, runCall);
+  node.lbserve(callKey, serverKey, process.env.IPCNAME, testy);
   return node;
 }
 export default init;
